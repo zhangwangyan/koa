0e45fa5befab3b57e6c8735c3c707ea52e14608c
desperado <17805272076@163.com>
2022-11-29 09:57:36 +0800

    redis.createClient(options)+redis+hget使用

---
+++ src/config/test.js
@@ -0,0 +1,11 @@
+import {getHValue,getValue,setValue} from "./RdisConfig";
+
+setValue('imooc','张王岩真帅')
+
+getValue('imooc').then((res)=>{
+    console.log('getValue'+res)
+})
+setValue('zhangwangyan',{name:'brain',age:30 })
+getHValue('zhangwangyan').then((res)=>{
+    console.log(res)
+})
